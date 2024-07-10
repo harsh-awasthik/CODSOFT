cc2b359d5a5a0598e3a9415b1cc945c0fe35c95c
Harsh Awasthi <harsh.awasthik@gmail.com>
2024-07-10 03:49:06 +0000

    .

--- todolist/todolist.py
+++ todolist/todolist.py
@@ -5,8 +5,13 @@ root.title("To Do List")
 root.geometry("900x1600")
 root.configure(background = "black")
 
+text = Entry(root, fg = "black", bg = "white")
+text.pack()
+text.insert(0, "Enter Your Task")
+
+
 def print_added():
-  lable = Label(root, text = "To Do list Updated", fg = "white", bg = "black")
+  lable = Label(root, text = text.get, fg = "white", bg = "black")
   lable.pack()
 
 add_new = Button(root, text = "+ add new", command = print_added, fg = "white", bg = "black")
